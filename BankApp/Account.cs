9a8242ffef4a5a6af01b4b05a31d6394b3ffd621
Grace Kim <gracekim0228@hotmail.com>
2019-03-17 11:27:11 -0700

    Static, constructor, object initialization and design pattern

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -11,18 +11,33 @@ namespace BankApp
     /// </summary>
     class Account
     {
+        #region statics
+        private static int lastAccountNumber = 0; 
+        #endregion
+
         #region Properties
         /// <summary>
         /// Unique number for the account
         /// </summary>
-        public int AccountNumber { get; set; }
+        public int AccountNumber { get; private set; }
         /// <summary>
         /// Email address of the account holder
         /// </summary>
         public string EmailAddress { get; set; }
-        public decimal Balance { get; set; }
+        public decimal Balance { get; private set; }
         public string AccountType { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; private set; }
+        #endregion
+
+        #region Constructor
+        public Account()
+        {
+            //lastAccountNumber++;
+            //AccountNumber = lastAccountNumber;
+
+            AccountNumber = ++lastAccountNumber;
+            CreatedDate = DateTime.Now;
+        }
         #endregion
 
         #region Methods
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -6,9 +6,14 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            var a1 = new Account();
-            a1.AccountNumber = 123456;
-            a1.Balance = 100000000;
+            
+            var a1 = Bank.CreateAccount("test@test.com", "Checking", 0);
+
+            Console.WriteLine($"AN: {a1.AccountNumber}, B: {a1.Balance}, CD: {a1.CreatedDate}, EA: {a1.EmailAddress}, AT: {a1.AccountType}");
+
+            var a2 = Bank.CreateAccount("test1@test.com", "Saving", 100);
+
+            Console.WriteLine($"AN: {a2.AccountNumber}, B: {a2.Balance}, CD: {a2.CreatedDate}, EA: {a2.EmailAddress}, AT: {a2.AccountType}");
         }
     }
 }
